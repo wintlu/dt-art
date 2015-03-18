284b3da1544680f49c2f7fe72678f69fea3d5ccd
wintlu <xidian.lumeng@gmail.com>
2015-03-18 13:52:24 +0800

    Add slider to home page

--- public/app.js
+++ public/app.js
@@ -11,6 +11,34 @@ $(document).ready(function(argument) {
         }
     }
 
+    function initSlider() {
+        if (window.$JssorSlider$) {
+            var options = {
+                $AutoPlay: true,
+                $FillMode: 2,
+                $ArrowNavigatorOptions: {
+                    $Class: $JssorArrowNavigator$,
+                    $ChanceToShow: 2,
+                    $AutoCenter: 2
+                }
+            };
+            var jssor_slider1 = new $JssorSlider$('slider_container', options);
+
+            function ScaleSlider() {
+                var bodyWidth = document.body.clientWidth;
+                if (bodyWidth)
+                    jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+                else
+                    window.setTimeout(ScaleSlider, 30);
+            }
+            ScaleSlider();
+
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+        }
+    }
+
     $('.modal-nav-toggle').click(function() {
         $('.modal-nav-wrap').addClass('active');
     });
@@ -29,4 +57,5 @@ $(document).ready(function(argument) {
 
     $(window).on('hashchange', hashChanged);
     hashChanged();
+    initSlider();
 });
